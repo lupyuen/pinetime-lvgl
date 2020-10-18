79f59dd740a786b66b7c1559a8acb2b4ca8b66b1
lupyuen <luppy@appkaki.com>
2020-10-18 10:45:31 +0800

    Added all widgets

--- src/widgets.rs
+++ src/widgets.rs
@@ -22,8 +22,206 @@ use crate::{
     },
 };
 
-/// Contains Rust bindings for LVGL Widgets API `lv_widgets`
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
 #[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
 #[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
 #[allow(unused_imports)]          //  Allow unused imports
-pub mod label;                    //  Export `widgets/label.rs` as Rust module `lvgl::widgets::label`
+pub mod arc;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod bar;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod btn;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod btnmatrix;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod calendar;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod canvas;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod chart;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod checkbox;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod cont;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod cpicker;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod dropdown;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod gauge;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod img;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod imgbtn;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod keyboard;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod label;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod led;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod line;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod linemeter;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod list;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod msgbox;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod objmask;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod objx_templ;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod page;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod roller;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod slider;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod spinbox;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod spinner;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod switch;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod table;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod tabview;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod textarea;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod tileview;
+
+/// Contains Rust bindings for LVGL Label Widget API `lv_label`
+#[allow(non_camel_case_types)]    //  Allow type names to have non-camel case
+#[allow(non_upper_case_globals)]  //  Allow globals to have lowercase letters
+#[allow(unused_imports)]          //  Allow unused imports
+pub mod win;
